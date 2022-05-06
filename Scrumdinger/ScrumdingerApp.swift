fd5347f182f407beb1deda57e631d77bf9da7c9b
Bruce Frick <bfrick22@gmail.com>
2022-05-05 20:46:11 -0700

    fisrt commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Bruce Frick on 5/3/22.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
